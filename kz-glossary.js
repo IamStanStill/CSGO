12f88f2b2b8a4e8e35562bfed040da0f37dff4b5
Stan <49242843+IamStanStill@users.noreply.github.com>
2021-05-11 16:47:24 +0100

    Update kz-glossary.js

--- kz-glossary.js
+++ kz-glossary.js
@@ -1,70 +1,70 @@
 /*
-forced perf	  			Map contains distances that are nearly impossible to reach with 275 prespeed
+forced perf       Map contains distances that are nearly impossible to reach with 275 prespeed
 hard tech  
 ladder-bhop chain		A series of consecutive ladders and bhops without any blocks to land on in between
 hard multihop			Contains long or difficult multihop sections
-kurouch					A bhop technique where you crouch bhop and turn around when you don't hit a perf
+kurouch					  A bhop technique where you crouch bhop and turn around when you don't hit a perf
 headbanger				When a player has to strafe out and duck at the right time to clear a low ceiling or obstacle
 timed ladder			A player can only remain on the ladder for a fixed amount of time. No checkpoints allowed
-weirdjump				Map forces a player to bhop after falling from a higher surface without jumping off first
-bhop					Player must use the scroll wheel to jump
-surf-bhop chain			A series of consecutive surfs and bhops without any blocks to land on in between
-comb					Easy maps with good flow. Most blocks can be bhopped
-run and crouch			Player must run forward and crouch at the right time to clear a gap.
-pixel-surf ski			Unintended skip that uses a pixel surf
+weirdjump				  Map forces a player to bhop after falling from a higher surface without jumping off first
+bhop					    Player must use the scroll wheel to jump
+surf-bhop chain		A series of consecutive surfs and bhops without any blocks to land on in between
+comb					    Easy maps with good flow. Most blocks can be bhopped
+run and crouch		Player must run forward and crouch at the right time to clear a gap.
+pixel-surf ski		Unintended skip that uses a pixel surf
 255 lj block			Map contains one or more numbered 255 lj blocks
 260 lj block			Map contains one or more numbered 260 lj blocks
 265 lj block			Map contains one or more numbered 265 lj blocks
-300 block				A numbered block that must be bhopped 
+300 block				  A numbered block that must be bhopped 
 cubby bhop				A player must bhop in and out of multiple constrained spaces
 180 ladder				A ladder where the destination block is directly behind the player facing the ladder
-gap surf				Multiple surf ramps separated by gaps that the player must strafe out of and back in to clear
-spikes					Contains sections where you have to bhop on spikes
+gap surf				  Multiple surf ramps separated by gaps that the player must strafe out of and back in to clear
+spikes					  Contains sections where you have to bhop on spikes
 strafeheavy				Map contains long distances that require good strafes to clear
-boxtech					When a player has to make abrupt 90 degree turns in the air
-gap jump				When a player has to jump through a small gap that's only slightly wider than the player model
-hard bhop				Map contains difficult singlehop or multihop sections
-headbanger bhop			Player must crouch while bhopping and cannot gain speed unless they hit a crouch perf
-booster					Mechanism that launches you upward when you jump on it
-climb					Easy map with a lot of climb
+boxtech					  When a player has to make abrupt 90 degree turns in the air
+gap jump				  When a player has to jump through a small gap that's only slightly wider than the player model
+hard bhop				  Map contains difficult singlehop or multihop sections
+headbanger bhop		Player must crouch while bhopping and cannot gain speed unless they hit a crouch perf
+booster					  Mechanism that launches you upward when you jump on it
+climb					    Easy map with a lot of climb
 weeb timer				Timer makes anime girl noises when used
 crouched headbanger		When a player has to jump out from inside a cubby from a crouched position
-dropdown				Strafe around obstacles or into small openings while falling
+dropdown				  Strafe around obstacles or into small openings while falling
 dropdown-bhop chain		A bhop section immediately following a dropdown
 rope ladders  
 hard ladder  
-hard singlehop			Contains long or difficult singlehop sections
-slide					A 1.6 game mode that contained smaller ramps and very technical surfs
-ladder-surf-bhop chain	A series of consecutive ladders, surfs, and bhops without any blocks to land on in between
-laj  
+hard singlehop		Contains long or difficult singlehop sections
+slide					    A 1.6 game mode that contained smaller ramps and very technical surfs
+ladder-surf-bhop chain	  A series of consecutive ladders, surfs, and bhops without any blocks to land on in between
+laj               Ladder jump
 ladder chain			A series of consecutive ladders without any blocks to land on in between
 crouchjump				When a player manually crouches and jumps in quick succession to gain extra height
-skip					Map containing one or more unintended skips
+skip					    Map containing one or more unintended skips
 surf reuse				Player needs to surf on a previous surf framp but from a higher platform to gain more height
 bhop crouch				A player must crouch before the peak of his jump to clear an obstacle or to reduce height
-timer tech skip			Map contains a timer that can be reached from an unintended location
+timer tech skip		Map contains a timer that can be reached from an unintended location
 ladder headbanger		Player must launch from a ladder and crouch at the right time to clear a lower ceiling
 cubby ladder			Player must ladder strafe out of a very constrained space with protruding sides
 small bhop				Player must bhop on very small blocks
-mini surf				A very abrupt surf ramp
+mini surf				  A very abrupt surf ramp
 mini ladder				A very short ladder
-tech surf				An inclined ramp where a player must run nearly parallel and jump with minimal mouse movement
+tech surf				  An inclined ramp where a player must run nearly parallel and jump with minimal mouse movement
 pro checkpoint zones	Map contains special zones a player must move into to set a pro checkpoint
 bhop to tech surf		Ride up a technical surf ramp while bhopping
-big surf				Big surf
+big surf				  Big surf
 drop strafe				Jump from a higher platform to a much lower one, often strafe heavy
 moving blocks			Map contains blocks that move
 268 lj block			Map contains one or more numbered 268 lj blocks
-porn room				Map containing NSFW room, usually hidden away
+porn room				  Map containing NSFW room, usually hidden away
 hard surf
 180 tech surf			An inclined ramp where the destination block is directly opposing it
 corner surf				A Player must surf around a 90 degree corner on to another surf
-resurf					Surfing a ramp more than once to gain speed
+resurf					  Surfing a ramp more than once to gain speed
 water surf				When a player holds jump and strafes on the water surface to gain speed
-keys					Map where a player needs to obtain an item to move on to the next level. 
-nonlinear				A map that contains multiple routes or objectives that can be finished in any order
+keys					    Map where a player needs to obtain an item to move on to the next level. 
+nonlinear				  A map that contains multiple routes or objectives that can be finished in any order
 slanted block jump		Player must jump from the high part of a block to a lower part of a slanted block above.
-ramp climb skip			Unintended skip where the player slowly climbs up an inclined surface
+ramp climb skip		Unintended skip where the player slowly climbs up an inclined surface
 old textures			Map contains 1.6 textures
 ladder around pillar	Two ladders on opposite sides of a pillar or wall
 wallstrafe				Gaining speed by holding a or d against the wall while moving parallel to it
@@ -72,14 +72,14 @@ gap ladder				Two ladders oriented vertically with a gap inbetween that the play
 no-collision blocks		Block is visibile but can't be stood on.
 ladder-surf chain		A series of consecutive ladders and surfs without any blocks to land on in between
 height surf				A small surf ramp that the player must use to get to a higher block
-gravity					Map manipulates gravity
-280 block				A numbered block that must be bhopped.
+gravity					  Map manipulates gravity
+280 block				  A numbered block that must be bhopped.
 crouch bhop				A player must manually crouch jump to a higher bhop block and immediately continue bhopping
 surf chain				Map contains multiple surfs in a row
 corridor jump			Similar to boxtech. Player must strafe and turn 90 degrees in a zigzag fashion through a tight corridor.
 small block				Player must strafe out from a small block and gain as much prespeed as possible
-blocked ladder			Two ladders oriented vertically with a block inbetween that the player must strafe out and back in to reach. 
-surf skip				Map containing an unintended surf skip
+blocked ladder		Two ladders oriented vertically with a block inbetween that the player must strafe out and back in to reach. 
+surf skip				  Map containing an unintended surf skip
 running headbanger		Player must run and jump at the right time to clear a low ceiling
 ramp climb				Player must slowly climb up an inclined surface by holding w
 invisible blocks		Block has collision but is invisible
